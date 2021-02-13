3baae79b1ece275cfc7cb40d6b9da53faf97103e
coolcao <me@coolcao.com>
2021-02-13 10:45:12 +0800

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# ~/.config/fish/config.fish
+
+# 启用starship
+#starship init fish | source
+
+
+# autojump
+if test -f /Users/coolcao/.autojump/share/autojump/autojump.fish; . /Users/coolcao/.autojump/share/autojump/autojump.fish; end
---
+++ fish/fish_prompt.fish
@@ -0,0 +1,16 @@
+# 该脚本为设置fish提示样式，直接在fish终端执行即可设置
+function fish_prompt --description 'coolcao prompt'
+    #Save the return status of the previous command
+    set -l last_pipestatus $pipestatus
+
+    switch "$USER"
+        case root toor
+            printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root and set_color $fish_color_cwd_root or set_color $fish_color_cwd) \ (prompt_pwd) (set_color normal)
+        case '*'
+            printf '\f[%s] %s%s in %s%s %s%s%s' (date "+%H:%M:%S") (set_color brblue) \ $USER  (set_color $fish_color_cwd) $PWD (set_color brblue)
+            fish_git_prompt (set_color yellow)
+            echo (set_color normal)
+            printf '➤ '
+    end
+end
+funcsave fish_prompt
