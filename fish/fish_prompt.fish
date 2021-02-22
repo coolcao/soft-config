69837b5bb52986e532d0791c0e77a1dad46d334a
coolcao <me@coolcao.com>
2021-02-22 16:37:06 +0800

    update fish config

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -1,4 +1,7 @@
 # 该脚本为设置fish提示样式，直接在fish终端执行即可设置
+# 大致样式如下：
+# [16:32:20]  coolcao in /Users/coolcao
+# ➤ ⏽
 function fish_prompt --description 'coolcao prompt'
     #Save the return status of the previous command
     set -l last_pipestatus $pipestatus
